2a81542b63fea7594c41fc10f118b9e9f0caa417
Rubu0423 <aoinoadoresu0423@gmail.com>
2022-10-23 12:29:10 +0900

    仮の弾と球を発射するスクリプト（仮）を追加しました

---
+++ SGCProject/Assets/Scripts/BalletFire.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalletFire : MonoBehaviour
+{
+    [SerializeField] 
+    GameObject Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) //左マウスボタンが押されたら
+        {
+            Debug.Log("押されてる");
+            Instantiate(Bullet, (transform.position), Quaternion.identity);
+        }
+    }
+}
